e014200ea195fb930725afbd68ebbd7b19900056
Peter Corcoran <petercorcoran1990@gmail.com>
2025-06-25 08:50:26 +0100

    Initial commit

---
+++ Java/eclipse-workspace/Week1/src/weekOne/InvoiceApplicationV1.java
@@ -0,0 +1,30 @@
+package weekOne;
+
+import java.util.Scanner;
+
+public class InvoiceApplicationV1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		// welcome the user to the program
+        System.out.println("Welcome to the Invoice Total Calculator");
+        System.out.println(); // print a blank line
+        
+		System.out.print("Enter subtotal: ");
+        double subtotal = sc.nextDouble();
+        System.out.print("Enter discount percent: ");
+        double discountPercent = sc.nextDouble();
+        double discountAmount = subtotal * discountPercent;
+        double total = subtotal - discountAmount;
+        
+        // display the discount amount and total
+        System.out.println("Subtotal:               " + subtotal);
+        System.out.println("Discount percent:       " + discountPercent);
+        System.out.println("Discount amount:        " + discountAmount);
+        System.out.println("Total:                  " + total);
+        
+        sc.close();
+	}
+
+}
